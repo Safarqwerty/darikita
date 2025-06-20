c6e05962994254add5c036ae027bba0dff208946
Zhaka Hidayat <zhakazx@gmail.com>
2025-06-20 19:42:47 +0800

    [config]: adding sidebar config

---
+++ config/sidebar.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+  [
+    'title' => 'Dashboard',
+    'route' => 'admin.dashboard'
+  ],
+  [
+    'title' => 'Kegiatan',
+    'route' => 'kegiatan.index'
+  ],
+  [
+    'title' => 'Donasi',
+    'route' => 'donasi.index'
+  ],
+  [
+    'title' => 'Pemberi Donasi',
+    'route' => 'pemberi-donasi.index'
+  ],
+  [
+    'title' => 'Pendaftaran',
+    'route' => 'pendaftaran.index'
+  ],
+  [
+    'title' => 'Manajemen User',
+    'route' => 'users.index'
+  ],
+];
