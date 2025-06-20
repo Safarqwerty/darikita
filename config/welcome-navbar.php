ec5d77f1bf50fe5842e5fec05a924df4df756820
Zhaka Hidayat <zhakazx@gmail.com>
2025-06-20 20:26:19 +0800

    [config]: adding welcome page navigation config

---
+++ config/welcome-navbar.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  [
+    'title' => 'Beranda',
+    'route' => '/',
+  ],
+  [
+    'title' => 'Kegiatan',
+    'route' => '#program',
+  ],
+  [
+    'title' => 'Impact',
+    'route' => '#impact'
+  ],
+  [
+    'title' => 'Testimoni',
+    'route' => '#testimonials'
+  ],
+];
